eb38cf5baab64cc924d53566a07050559b808abf
Rafael Correia <rafael.correia@soulisto.com.br>
2019-10-02 19:35:55 -0300

    first commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.3.50"
+    application
 }
 
 group = "io.github.rafaeljpc"
@@ -10,6 +11,10 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
     jcenter()
+}
+
+application {
+    mainClassName = "io.github.rafaeljpc.tutorial.spark.MainKt"
 }
 
 dependencies {
